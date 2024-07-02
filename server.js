6fe8639b78b6256efa3177efc9495e562fac6e1a
Paul-Wisdom <wisdomp837@gmail.com>
2024-07-02 12:36:33 +0100

    testing

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('dotenv').config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.get('/api/hello', (req, res, next) => {
+    const name = req.query.visitor_name
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    return res.json({client_ip: ip, message: `hello ${name}`});
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
